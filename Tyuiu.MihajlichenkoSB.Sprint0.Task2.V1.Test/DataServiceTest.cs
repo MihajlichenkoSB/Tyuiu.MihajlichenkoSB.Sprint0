1459210bff31b4b87f1132c8f30cb781ae95f8f9
Сергей Михайличенко <Tyuiu.mihajlichenkosb@outlook.com>
2025-09-13 14:08:04 +0500

    Task2

---
+++ Tyuiu.MihajlichenkoSB.Sprint0.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint0.Task2.V1.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint0.Task2.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekGetMessageValid()
+        {
+            var name = "Сергей";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Сергей", res);
+        }
+    }
+}
---
+++ Tyuiu.MihajlichenkoSB.Sprint0.Task2.V1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint0.Task2.V1.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint0.Task2.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Сергей"));
+            Console.ReadKey();
+        }
+    }
+}
