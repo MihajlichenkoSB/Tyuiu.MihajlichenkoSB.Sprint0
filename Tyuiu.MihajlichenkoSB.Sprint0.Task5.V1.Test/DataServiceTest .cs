68c8a8d5d0de19f17b7d61321bcd164f0f2eb99a
Сергей Михайличенко <Tyuiu.mihajlichenkosb@outlook.com>
2025-09-13 17:39:27 +0500

    Task5

---
+++ Tyuiu.MihajlichenkoSB.Sprint0.Task5.V1.Test/DataServiceTest .cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint0.Task5.V1.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint0.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.MihajlichenkoSB.Sprint0.Task5.V1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint0.Task5.V1.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint0.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B =" + DataService.Addition(1, 5));
+            Console.WriteLine("A - B =" + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B =" + DataService.Multiplication(10, 10));   
+            Console.WriteLine("A / B =" + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
