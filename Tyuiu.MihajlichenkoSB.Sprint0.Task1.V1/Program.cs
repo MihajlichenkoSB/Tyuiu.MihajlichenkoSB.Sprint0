271fa751f4c7104bab9029cea019048497b60728
Сергей Михайличенко <Tyuiu.mihajlichenkosb@outlook.com>
2025-09-13 11:31:59 +0500

    Task1

---
+++ Tyuiu.MihajlichenkoSB.Sprint0.Task1.V1/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    public Program()
+    {
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+        string name = "Сергей";
+        int age = 17;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + " Мне " + age + "Лет");
+        Console.ReadKey();
+    }
+}
